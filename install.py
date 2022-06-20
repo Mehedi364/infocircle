d13bf547edc9b9ca5d619a0dbb281bdf70a9f683
Mohammad Alamin <107840651+AKXVAU@users.noreply.github.com>
2022-06-21 03:14:21 +0600

    Create install.py

---
+++ install.py
@@ -0,0 +1,8 @@
+import os, sys
+os.system("clear")
+os.system("pip3 install infocircle==1.0.2")
+os.system("clear)
+print("INFOCIRCLE INSTALL SUCCESSFUL")
+print("JUST OPEN YOUR TERMUX & TYPE : infocircle FOR RUN THIS TOOL")
+os.system("clear")
+sys.exit()
